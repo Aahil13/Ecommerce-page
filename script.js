0d001329acb3889b8c8f73a4fef58f53582d9bbc
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-05-31 03:45:47 +0100

    completed the cart functionality and started app responsitivity

--- script.js
+++ script.js
@@ -7,23 +7,7 @@ const rightNav = document.querySelector(".right-nav");
 const cart = document.querySelector(".cart");
 const addToCartBtn = document.querySelector(".addToCartBtn");
 const numberOfItemsAdded = document.querySelector(".numberOfItemsAdded");
-
-const cartItem = `
-  <div class="cartItems">
-          <div class="cartItemImg">
-            <img src="images/image-product-1-thumbnail.jpg" alt="img-thumbnails">
-          </div>
-          <div class="cartItemPrice">
-            <div class="cartItemTag">
-              <p>Fall Limited Edition Sneakers</p>
-              <p>$125.00 × 3<strong style="margin-: .5rem;"> $375.00</strong></p>
-            </div>
-          </div>
-          <div class="trashIcon">
-            <img src="images/icon-delete.svg" alt="trashIcon">
-          </div>
-        </div>
-`;
+const cartText = document.querySelector(".cartText");
 
 //state Values
 let initialNumber = 0;
@@ -40,11 +24,50 @@ decreaseItems.addEventListener("click", () => {
   numberOfItems.innerHTML = initialNumber;
 });
 
+const cartItem = function (initialNumber) {
+  return `
+           <div class="txt">
+            <div class="cartItems">
+              <div class="cartItemImg">
+                <img
+                  src="images/image-product-1-thumbnail.jpg"
+                  alt="img-thumbnails"
+                />
+              </div>
+              <div class="cartItemPrice">
+                <div class="cartItemTag">
+                  <p>Fall Limited Edition Sneakers</p>
+                  <p>
+                    $125.00 × ${initialNumber} <strong style="margin-: 0.5rem"> $${125.00 * initialNumber}.00</strong>
+                  </p>
+                </div>
+              </div>
+              <div class="trashIcon">
+                <img src="images/icon-delete.svg" alt="trashIcon" />
+              </div>
+            </div>
+          </div>
+          <div class="btn">
+            <div class="buttons cartBtn">
+              <img src="images/icon-cart-white.svg" alt="cart" />
+              <p>checkout</p>
+            </div>
+          </div>
+`;
+};
+
+const defaultText = `<p>Your page is empty</p>`;
+
 //toggling the cart
 const showCart = function () {
   rightNav.addEventListener("click", (e) => {
     if (e.target.tagName !== "IMG") return;
     cart.classList.toggle("display");
+
+    if (initialNumber === 0) {
+      cartText.innerHTML = "";
+      cartText.insertAdjacentHTML("afterbegin", defaultText);
+    }
   });
 };
 showCart();
@@ -54,10 +77,20 @@ addToCartBtn.addEventListener("click", () => {
   //takes the current number of items and adds it to the cart icon
   numberOfItemsAdded.style.display = "block";
   numberOfItemsAdded.style.color = "#fff";
-  numberOfItemsAdded.innerHTML = numberOfItems.innerHTML;
+  numberOfItemsAdded.innerHTML = initialNumber;
 
-  //adds Items to the cart and adds button
-  if (numberOfItems.innerHTML !== 0) {
-    cart.classList.toggle("display");
+  if (initialNumber === 0) {
+    cartText.innerHTML = "";
+    cartText.insertAdjacentHTML("afterbegin", defaultText);
+  } else {
+    cartText.innerHTML = "";
+    cartText.insertAdjacentHTML("afterbegin", cartItem(initialNumber));
   }
+
+  const trashIcon = document.querySelector(".trashIcon");
+  trashIcon.addEventListener('click', ()=>{
+   numberOfItemsAdded.style.display = 'none'
+   cartText.innerHTML = "";
+   cartText.insertAdjacentHTML("afterbegin", defaultText);
+  })
 });
